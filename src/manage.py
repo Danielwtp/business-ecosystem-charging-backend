03644213ab73cd43ead600ea7f2814f7b33c8448
Dano <danielcswtp@gmail.com>
2019-07-26 13:20:36 +0200

    little changes

--- src/manage.py
+++ src/manage.py
@@ -2,8 +2,8 @@
 import os
 import sys
 
-def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+if __name__ == '__main__':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -13,7 +13,3 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
